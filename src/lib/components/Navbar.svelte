edbce38c7103ac7a77d44918fc374b001b547ad7
William Liu <orangetnt827@gmail.com>
2024-10-18 20:26:06 -0500

    hopefully fix broken homelink

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div class="text-gray-100 text-2xl h-auto p-4 bg-gray-900 text-center p-auto flex flex-col items-center justify-center">
-  <a href="{base}" class="text-3xl border-solid border-2 border-white p-2 mb-4 rounded-lg max-w-fit">
+  <a href="{base}/" class="text-3xl border-solid border-2 border-white p-2 mb-4 rounded-lg max-w-fit">
     <h2>William Liu</h2>
   </a>
   <nav class="sm:space-x-8 flex flex-col sm:flex-row items-center cursor-pointer">
-      <a href="{base}">HOME</a>
+      <a href="{base}/">HOME</a>
       <a href="{base}/awards">AWARDS</a>
       <a href="{base}/leadership">LEADERSHIP</a>
       <a href="{base}/projects">PROJECTS</a>
